e70eac47b625f5ed5571d2ba714782990fd30090
RachaelDugan <118020235+RachaelDugan@users.noreply.github.com>
2022-12-29 17:57:49 -0700

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    internal class WordSmith
+    {
+        public bool IsAPalindrome(string word) 
+        {
+            int min = 0;
+            int max = word.Length - 1;
+            while (true)
+            {
+                if (min > max)
+                {
+                    return true;
+                }
+                char a = word[min];
+                char b = word[max];
+                if (char.ToLower(a) != char.ToLower(b))
+                {
+                    return false;
+                }
+                min++;
+                max--;
+            }
+           
+        }
+        
+    }
+}
